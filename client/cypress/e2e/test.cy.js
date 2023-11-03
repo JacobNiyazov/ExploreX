ff61485c7022c1e73f5af3578a062b8a4cbbbf39
jeo02 <70209456+jeo02@users.noreply.github.com>
2023-11-03 18:09:51 -0400

    Added correctly formatted cypress tests

---
+++ client/cypress/e2e/test.cy.js
@@ -0,0 +1,7 @@
+describe("Automation using Cypress", () => {
+    it("Open website and enter username, password", () => {
+      cy.visit(
+        "https://ecommerce-playground.lambdatest.io/index.php?route=account/login"
+      );
+    });
+   });
