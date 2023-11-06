7c2f5cd378deb11d4de74e16b88e3b8455f7e7fe
Jesse Wang <jesse.wang@stonybrook.edu>
2023-11-06 13:26:52 -0500

    break test

--- client/cypress/e2e/firstTest.cy.js
+++ client/cypress/e2e/firstTest.cy.js
@@ -2,6 +2,6 @@ describe('Text Presence Test', () => {
   it('Check if ExploreX is on the Screen', () => {
     cy.visit('http://localhost:3000'); 
 
-    cy.contains('ExploreX').should('be.visible'); 
+    cy.contains('iphone').should('be.visible'); 
   });
 });
