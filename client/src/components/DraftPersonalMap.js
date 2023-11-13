93a4ca1129c9aab32937b97cea3c907908c95291
jeo02 <70209456+jeo02@users.noreply.github.com>
2023-11-13 13:14:59 -0500

    Update DraftPersonalMap.js

--- client/src/components/DraftPersonalMap.js
+++ client/src/components/DraftPersonalMap.js
@@ -33,6 +33,7 @@ function DraftPersonalMap(map){
                 <Button
                     sx = {{marginBottom: "2vh",
                     marginLeft: "3vw", color: "white"}}
+                    data-testid="EditScreenButton"
                     onClick={handleEditClick}
                     >
                         <ModeEditIcon></ModeEditIcon>
