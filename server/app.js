5a881c61dd40239782ae23f8658786c88f66e797
Jesse Wang <jesse.wang@stonybrook.edu>
2023-11-15 10:49:46 -0500

    changes to auth

--- server/app.js
+++ server/app.js
@@ -18,56 +18,16 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(cookieParser())
 
-// connect MongoDB
-// mongoose.connect(process.env.MONGODB_URI).then(() => {
-//   const PORT = process.env.PORT || 8000
-//   app.listen(PORT, () => {
-//       console.log(`App is Listening on PORT ${PORT}`);
-//   })
-// }).catch(err => {
-//   console.log(err);
-// });
-
 const PORT = process.env.PORT || 8000
 
 // INITIALIZE OUR DATABASE OBJECT
 const db = require('./db')
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-app.use(express.static(path.join(__dirname, '../client/build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/build'))
-})
-
 // PUT THE SERVER IN LISTENING MODE
 const server = app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
-
-const Item = mongoose.model('Item', { name: String });
-
-app.put('/api/items', async (req, res) => {
-  try {
-    const { name } = req.body;
-    const item = new Item({ name });
-    await item.save();
-    res.status(201).json(item);
-  } catch (error) {
-    res.status(500).json({ error: 'Server error' });
-  }
-});
-
-app.get('/api/items', async (req, res) => {
-  try {
-    const items = await Item.find();
-    res.status(200).json(items);
-  } catch (error) {
-    res.status(500).json({ error: 'Server error' });
-  }
-});
-
-// route
-app.get("/api", (req, res) => {
-    res.status(201).json({message: "Welcome& to Backend!"});
-});
+const authRouter = require('./routes/auth-router')
+app.use('/auth', authRouter)
 
 module.exports = {server,app}
