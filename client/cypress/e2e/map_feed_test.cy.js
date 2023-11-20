578a2ecee4c93f07b053bb49c3f0933b6246c31b
Jacob Niyazov <jacobniyazov@yahoo.com>
2023-11-20 11:49:08 -0500

    cypress fe test

--- client/cypress/e2e/map_feed_test.cy.js
+++ client/cypress/e2e/map_feed_test.cy.js
@@ -10,14 +10,48 @@ describe('Map Feed and Public Map View Test', () => {
   it('leads to public view page and likes and dislikes', ()=>{
     cy.get('[data-testid=map-feed-card-0]').click();
     cy.get('[data-testid=public-map-view]').first().within(()=>{
-      cy.get('[data-testid=map-like-button]').click();
-      cy.get('[data-testid=map-likes-count]').should('have.text', '4');
+      cy.get('[data-testid=map-likes-count]')
+        .invoke('text')
+        .then((initialLikesText) => {
+          const initialLikes = parseInt(initialLikesText, 10);
+          cy.get('[data-testid=map-like-button]')
+            .click();
 
-      cy.get('[data-testid=map-dislike-button]').click();
-      cy.get('[data-testid=map-dislikes-count]').should('have.text', '17');
-      cy.get('[data-testid=map-dislike-button]').click();
-      cy.get('[data-testid=map-dislikes-count]').should('have.text', '16');
-      cy.get('[data-testid=map-likes-count]').should('have.text', '3');
+          cy.get('[data-testid=map-likes-count]')
+            .invoke('text')
+            .should('equal', (initialLikes + 1).toString());
+      });
+      cy.get('[data-testid=map-dislikes-count]')
+        .invoke('text')
+        .then((initialDislikesText) => {
+          cy.get('[data-testid=map-likes-count]')
+            .invoke('text')
+            .then((initialLikesText) => {
+              const initialLikes = parseInt(initialLikesText, 10);
+
+              const initialDislikes = parseInt(initialDislikesText, 10);
+              cy.get('[data-testid=map-dislike-button]')
+                .click();
+
+              cy.get('[data-testid=map-dislikes-count]')
+                .invoke('text')
+                .should('equal', (initialDislikes + 1).toString());
+              cy.get('[data-testid=map-likes-count]')
+                .invoke('text')
+                .should('equal', (initialLikes - 1).toString());
+          });
+      });
+      cy.get('[data-testid=map-dislikes-count]')
+        .invoke('text')
+        .then((initialDislikesText) => {
+          const initialDislikes = parseInt(initialDislikesText, 10);
+          cy.get('[data-testid=map-dislike-button]')
+            .click();
+
+          cy.get('[data-testid=map-dislikes-count]')
+            .invoke('text')
+            .should('equal', (initialDislikes - 1).toString());
+      });
     })
   })
   it('leads to public map view and leaves a comment', ()=>{
